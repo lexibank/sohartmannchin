5bca4351e7b4aea153ec3ad7feacd55ffabb7153
chrzyki <christoph@functor-argument.net>
2019-11-11 14:20:29 +0100

    Added tests, refactorings, re-generated CLDF

--- lexibank_sohartmannchin.py
+++ lexibank_sohartmannchin.py
@@ -1,20 +1,22 @@
 from pathlib import Path
 from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank.util import pb
+from pylexibank.util import progressbar
 from pylexibank import Concept, Language
 
 from clldutils.misc import slug
 import attr
 from lingpy import *
 
+
 @attr.s
 class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     ChineseName = attr.ib(default=None)
-    SubGroup = attr.ib(default='Chin')
-    Family = attr.ib(default='Sino-Tibetan')
+    SubGroup = attr.ib(default="Chin")
+    Family = attr.ib(default="Sino-Tibetan")
     DialectGroup = attr.ib(default=None)
+
 
 @attr.s
 class CustomConcept(Concept):
@@ -29,16 +31,15 @@ class Dataset(BaseDataset):
     def cmd_makecldf(self, args):
 
         args.writer.add_sources()
-        language_lookup = args.writer.add_languages(
-                lookup_factory="Name")
+        language_lookup = args.writer.add_languages(lookup_factory="Name")
         concept_lookup = args.writer.add_concepts(
-                id_factory=lambda x: x.id.split('-')[-1]+'_'+slug(x.english),
-                lookup_factory='Name')
+            id_factory=lambda x: x.id.split("-")[-1] + "_" + slug(x.english), lookup_factory="Name"
+        )
         wl = Wordlist(self.raw_dir.joinpath("HSH-SCL.csv").as_posix())
-        for idx in pb(wl):
+        for idx in progressbar(wl):
             args.writer.add_forms_from_value(
                 Language_ID=language_lookup[wl[idx, "language"]],
                 Value=wl[idx, "reflex"],
                 Source=["SoHartmann1988"],
-                Parameter_ID=concept_lookup[wl[idx, "concept"]]
+                Parameter_ID=concept_lookup[wl[idx, "concept"]],
             )
--- setup.py
+++ setup.py
@@ -1,31 +1,20 @@
 from setuptools import setup
-import sys
 import json
 
 
-with open('metadata.json', encoding='utf-8') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_sohartmannchin',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_sohartmannchin'],
+    name="lexibank_sohartmannchin",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_sohartmannchin"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'sohartmannchin=lexibank_sohartmannchin:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.0.0',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["sohartmannchin=lexibank_sohartmannchin:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
